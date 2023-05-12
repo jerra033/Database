f9af174964a806fd5f0a13cdf60ac9471d98339c
jeremiah <jeremiahlaryea6@gmail.com>
2023-05-12 15:07:29 +0200

    zemel

--- index.php
+++ index.php
@@ -1,8 +1,41 @@
-<?php
-$test = 12;
-$test = 10;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welkom op deze pagina</h1>
+    <?php echo "goodbye world"; ?>
 
-$test = 12;
+    <?php
+$int1 = 12;
+$int2 = 10;
+echo $int1+$int2;
+?>
+
+<?php
+$int1 = 12;
+$int2 = 10;
+$resultaat = $int1 * $int2;
+?>
+
+<?php
+$naam="jerre";
+echo "welkom " .$naam;
+?>
+
+<?php
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam ." zit bij ". $jouwNaam  ." in de klas!"
 
 ?>
+
+</body>
+</html>
+
 
